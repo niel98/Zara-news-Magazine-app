a27246cce04ce0d74b8947e12af17eaba274a4c6
Niel <mosesdaniel201798@gmail.com>
2021-12-07 11:48:06 +0100

    query feature added

--- controller/news.js
+++ controller/news.js
@@ -1,8 +1,19 @@
 const News = require('../model/News')
+const escapeString = require('escape-string-regexp')
 
-const getNews = async (_, res) => {
+const getNews = async (req, res) => {
     try {
-        const news = await News.find()
+        let query = {}
+        if (Object.keys(req.query).length) {
+            for (let value in req.query) {
+                const $regex = '^'+ escapeString(req.query[value])+'$'
+                const $options = 'i'
+                query[value] = {$regex, $options}
+            }
+        }
+        
+        const news = await News.find(query)
+        console.log('Query: ', req.query.category)
         console.log('News: ', news)
         res.status(200).json({ success: true, data: news })
     } catch (error) {   
