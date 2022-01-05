46d30cf101c8f8d1a9e695a683ccc1356913eb11
Chukwuka Ezeaka <chukwuka.ezeaka1@outlook.com>
2022-01-05 13:38:19 +0100

    updated default value for count field

--- model/News.js
+++ model/News.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const NewsSchema = new Schema({
-    avatar: {
-        type: String
-    },
-    cloudinary_id: {
-        type: String
-    },
-    title: {
-        type: String
-    },
-    content: {
-        type: String
-    },
-    category: {
-        type: String
-    },
-    count: {
-        type: Number,
-        default: count = 0
-    },
-    Date: {
-        type: Date,
-        default: Date.now()
-    }
-})
+  avatar: {
+    type: String,
+  },
+  cloudinary_id: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  count: {
+    type: Number,
+    default: 0,
+  },
+  Date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-const News = mongoose.model('News', NewsSchema)
+const News = mongoose.model("News", NewsSchema);
 
-module.exports = News
+module.exports = News;
