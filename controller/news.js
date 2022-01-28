8f2562a727c8770ec365cb6758350d0e238abc7d
Niel <mosesdaniel201798@gmail.com>
2022-01-28 15:12:16 +0100

    payment check on hold in production

--- controller/news.js
+++ controller/news.js
@@ -53,18 +53,18 @@ const increaseUserNewsCount = async(req, res) => {
   await device_id.save()
 
   //Check the device expiry date
-  if (device_id.subscription.expiresIn && new Date().getTime() < new Date(device_id.subscription.expiresIn).getTime()) {
-    device_id.subscription.isSubscribed = false
+//   if (device_id.subscription.expiresIn && new Date().getTime() < new Date(device_id.subscription.expiresIn).getTime()) {
+//     device_id.subscription.isSubscribed = false
 
-    await device_id.save()
-}
+//     await device_id.save()
+// }
 
-  console.log('Subscription status: ', device_id.subscription.isSubscribed)
-  console.log('Subscription Expiry date: ', device_id.subscription.expiresIn)
+//   console.log('Subscription status: ', device_id.subscription.isSubscribed)
+//   console.log('Subscription Expiry date: ', device_id.subscription.expiresIn)
 
-  if (device_id.count >= 5 && !device_id.subscription.isSubscribed) {
-    return res.send('Please Subscribe to continue reading the news articles')
-  }
+//   if (device_id.count >= 5 && !device_id.subscription.isSubscribed) {
+//     return res.send('Please Subscribe to continue reading the news articles')
+//   }
 
   const news = await News.findById(req.params.newsId)
     if (!news) {
